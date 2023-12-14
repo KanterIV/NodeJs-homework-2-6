5e410ee4d7ab5731a7b52f8f53eab6636122bed1
KanterIV <126972573+KanterIV@users.noreply.github.com>
2023-12-14 14:29:38 +0100

    controller logic is adapted to mongoDB
    - added mongoose schem and model;
    -changed joi schem +new schem for "favorite";
    -controller logic is adapted to mongoDB;
    -added new controller (updateStatusContact);

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,15 +1,15 @@
 const { HttpError, ctrlWrapper } = require("./../helpers");
 
-const contacts = require("../models/contacts");
+const { Contact } = require("../models/contact");
 
 const getAllContacts = async (req, res) => {
-  const allContacts = await contacts.listContacts();
+  const allContacts = await Contact.find();
   res.json(allContacts);
 };
 
 const getContactById = async (req, res) => {
   const { contactId } = req.params;
-  const contact = await contacts.getContactById(contactId);
+  const contact = await Contact.findById(contactId);
   if (!contact) {
     throw HttpError(404, "Not found");
   }
@@ -17,13 +17,13 @@ const getContactById = async (req, res) => {
 };
 
 const addContact = async (req, res) => {
-  const newContact = await contacts.addContact(req.body);
+  const newContact = await Contact.create(req.body);
   res.status(201).json(newContact);
 };
 
 const removeContact = async (req, res) => {
   const { contactId } = req.params;
-  const contact = await contacts.removeContact(contactId);
+  const contact = await Contact.findByIdAndDelete(contactId);
   if (!contact) {
     throw HttpError(404, "Not found");
   }
@@ -32,7 +32,24 @@ const removeContact = async (req, res) => {
 
 const updateContact = async (req, res) => {
   const { contactId } = req.params;
-  const updatedContactById = await contacts.updateContact(contactId, req.body);
+  const updatedContactById = await Contact.findByIdAndUpdate(
+    contactId,
+    req.body,
+    { new: true }
+  );
+  if (!updatedContactById) {
+    throw HttpError(404, "Not Found");
+  }
+  res.status(200).json(updatedContactById);
+};
+
+const updateStatusContact = async (req, res) => {
+  const { contactId } = req.params;
+  const updatedContactById = await Contact.findByIdAndUpdate(
+    contactId,
+    req.body,
+    { new: true }
+  );
   if (!updatedContactById) {
     throw HttpError(404, "Not Found");
   }
@@ -45,4 +62,5 @@ module.exports = {
   addContact: ctrlWrapper(addContact),
   removeContact: ctrlWrapper(removeContact),
   updateContact: ctrlWrapper(updateContact),
+  updateStatusContact: ctrlWrapper(updateStatusContact),
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,20 +2,32 @@ const express = require("express");
 
 const ctrl = require("../../controllers/contacts");
 
-const { validateBody } = require("../../midlwares");
+const { validateBody, isValidId } = require("../../midlwares");
 
-const { addSchema } = require("../../schemas");
+const { schemes } = require("../../models/contact");
 
 const router = express.Router();
 
 router.get("/", ctrl.getAllContacts);
 
-router.get("/:contactId", ctrl.getContactById);
+router.get("/:contactId", isValidId, ctrl.getContactById);
 
-router.post("/", validateBody(addSchema), ctrl.addContact);
+router.post("/", validateBody(schemes.addSchema), ctrl.addContact);
 
-router.delete("/:contactId", ctrl.removeContact);
+router.delete("/:contactId", isValidId, ctrl.removeContact);
 
-router.put("/:contactId", validateBody(addSchema), ctrl.updateContact);
+router.put(
+  "/:contactId",
+  isValidId,
+  validateBody(schemes.addSchema),
+  ctrl.updateContact
+);
+
+router.patch(
+  "/:contactId/favorite",
+  isValidId,
+  validateBody(schemes.updateFavoriteSchema),
+  ctrl.updateStatusContact
+);
 
 module.exports = router;
