bb38439c90b756ac03afe8774cd71b9781a90cf6
KanterIV <126972573+KanterIV@users.noreply.github.com>
2023-12-18 18:05:50 +0100

    Added register and login logic
    - Added new routes ("/register"and "/login");
    -Added register and login contollers

---
+++ controllers/auth.js
@@ -0,0 +1,49 @@
+const { HttpError, ctrlWrapper } = require("./../helpers");
+
+const bcrypt = require("bcrypt");
+
+const { User } = require("../models/user");
+const { response } = require("../app");
+
+const register = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user) {
+    throw HttpError(409, "Email in use");
+  }
+
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({ ...req.body, password: hashPassword });
+  res.status(201).json({
+    user: { email: newUser.email, subscription: newUser.subscription },
+  });
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+
+  const passwordCompare = await bcrypt.compare(password, user.password);
+
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+
+  const token = "example.qwerty.example";
+
+  res.status(200).json({
+    token: token,
+    user: { email: user.email, subscription: user.subscription },
+  });
+};
+
+module.exports = {
+  register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
+};
